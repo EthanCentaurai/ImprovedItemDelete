6a1bd4933cffd450009718d92e9e6fd5ba601650
Ethan Centaurai <ethancentaurai@outlook.com>
2018-07-27 20:07:07 +0100

    Initial commit

---
+++ ImprovedItemPrompt.lua
@@ -0,0 +1,6 @@
+
+StaticPopupDialogs["DELETE_ITEM"].enterClicksFirstButton = 1
+StaticPopupDialogs["DELETE_QUEST_ITEM"].enterClicksFirstButton = 1
+
+StaticPopupDialogs["DELETE_GOOD_ITEM"] = StaticPopupDialogs["DELETE_ITEM"]
+StaticPopupDialogs["DELETE_GOOD_QUEST_ITEM"] = StaticPopupDialogs["DELETE_QUEST_ITEM"]
