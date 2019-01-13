a77960c0a3e27b3bc56de111fcf2e14fd07b9deb
Ethan Centaurai <ethancentaurai@outlook.com>
2019-01-13 01:06:24 +0000

    Press Enter to equip items.

--- ImprovedItemPrompt.lua
+++ ImprovedItemPrompt.lua
@@ -1,6 +1,11 @@
 
+-- delete items
 StaticPopupDialogs["DELETE_ITEM"].enterClicksFirstButton = 1
 StaticPopupDialogs["DELETE_QUEST_ITEM"].enterClicksFirstButton = 1
 
 StaticPopupDialogs["DELETE_GOOD_ITEM"] = StaticPopupDialogs["DELETE_ITEM"]
 StaticPopupDialogs["DELETE_GOOD_QUEST_ITEM"] = StaticPopupDialogs["DELETE_QUEST_ITEM"]
+
+-- equip items
+StaticPopupDialogs["EQUIP_BIND"].enterClicksFirstButton = 1
+StaticPopupDialogs["EQUIP_BIND_TRADEABLE"].enterClicksFirstButton = 1
